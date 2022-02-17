ee5d2f53a4d070e7caadb792ded5e145dc939c90
Cesar Salazar <bitemecesar@gmail.com>
2022-02-17 02:34:50 -0400

    Fix: delete unused vars (running server pipeline)

--- pages/payment/confirmation.tsx
+++ pages/payment/confirmation.tsx
@@ -1,29 +1,30 @@
 import * as React from 'react';
 import { Layout } from 'components/layout';
-import { Form, Input, Select, Button } from 'antd';
+import { Form, Button } from 'antd';
 import { Divider } from 'components/divider';
 import { Images } from 'interfaces';
 import Link from 'next/link';
 
 const PaymentBilling = () => {
-	
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const [form] = Form.useForm();
-	const onFinish = (values: any) => {
-		console.log('Success:', values);
-	};
 
 	return (
 		<Layout withHeader withFooter className="flex flex-row">
-
-
 			{/*Left side*/}
 			<div className="flex flex-col w-1/2 p-10">
 				<div className="flex flex-col items-center justify-center bg-white p-10">
-					<img src={Images.order_confirmed_picture}/>
-					<h1 className="text-2xl text-black pb-12 text-modified mt-10">Order Confirmed</h1>
-					<h3 className="text-base text-black pb-12 text-modified">Your order have been confirmed, please wait and track your order</h3>
+					<img src={Images.order_confirmed_picture} />
+					<h1 className="text-2xl text-black pb-12 text-modified mt-10">
+						Order Confirmed
+					</h1>
+					<h3 className="text-base text-black pb-12 text-modified">
+						Your order have been confirmed, please wait and track your order
+					</h3>
 					<Link href={'/'}>
-						<Button className="px-5 py-3" htmlType="submit">Go to track page</Button>
+						<Button className="px-5 py-3" htmlType="submit">
+							Go to track page
+						</Button>
 					</Link>
 				</div>
 			</div>
@@ -38,9 +39,13 @@ const PaymentBilling = () => {
 					<div className="flex flex-row">
 						<div className="flex flex-col w-8/12">
 							<h3 className="text-base text-black">Way Kambas Mini Ebony</h3>
-							<h3 className="text-base text-gray-400 pb-2">2 x IDR 1.024.000</h3>
+							<h3 className="text-base text-gray-400 pb-2">
+								2 x IDR 1.024.000
+							</h3>
 							<h3 className="text-base text-black">Sikka (Ebony & Mapple)</h3>
-							<h3 className="text-base text-gray-400 pb-6">1 x IDR 1.264.000</h3>
+							<h3 className="text-base text-gray-400 pb-6">
+								1 x IDR 1.264.000
+							</h3>
 						</div>
 						<div className="w-4/12"></div>
 					</div>
@@ -56,15 +61,23 @@ const PaymentBilling = () => {
 						<h3 className="text-2xl text-black pb-6 w-1/2">Packaging</h3>
 						<h3 className="text-2xl text-black pb-6 w-1/2">Rp 50.000</h3>
 					</div>
-					<Divider custom_divider_color="#FF6C00" className="bottom-[10px] w-full"/>
+					<Divider
+						custom_divider_color="#FF6C00"
+						className="bottom-[10px] w-full"
+					/>
 					<div className="flex flex-row mt-6">
 						<h3 className="text-2xl text-black pb-6 w-1/2">Grand Total</h3>
 						<h3 className="text-3xl text-black pb-6 w-1/2">Rp 2.702.000</h3>
 					</div>
-					<Divider custom_divider_color="#FF6C00" className="bottom-[10px] w-full"/>
+					<Divider
+						custom_divider_color="#FF6C00"
+						className="bottom-[10px] w-full"
+					/>
 					<div className="flex flex-row mt-6">
 						<h3 className="text-2xl text-black pb-6 w-1/2">Shipping Address</h3>
-						<h3 className="text-2xl text-black pb-6 w-1/2">18 Richardson Drive Fountain Valley, CA 92708</h3>
+						<h3 className="text-2xl text-black pb-6 w-1/2">
+							18 Richardson Drive Fountain Valley, CA 92708
+						</h3>
 					</div>
 				</div>
 			</div>
--- pages/payment/payment.tsx
+++ pages/payment/payment.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react';
 import { Layout } from 'components/layout';
-import { Form, Input, Select, Button } from 'antd';
+import { Form, Button } from 'antd';
 import { Divider } from 'components/divider';
 import Link from 'next/link';
 
 const PaymentBilling = () => {
-	
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const [form] = Form.useForm();
-	const onFinish = (values: any) => {
-		console.log('Success:', values);
-	};
 
 	return (
 		<Layout withHeader withFooter className="flex flex-col">
@@ -18,7 +15,9 @@ const PaymentBilling = () => {
 				<div className="w-1/2 p-10 flex flex-col">
 					{/*Detail Order*/}
 					<div className="p-10 bg-white mb-10">
-						<h1 className="text-3xl text-black pb-12 font-bold">Detail Order</h1>
+						<h1 className="text-3xl text-black pb-12 font-bold">
+							Detail Order
+						</h1>
 						<div className="flex flex-row">
 							<h3 className="text-2xl text-black pb-6 w-1/2">Subtotal</h3>
 							<h3 className="text-2xl text-black pb-6 w-1/2">Rp 2.152.000</h3>
@@ -31,48 +30,74 @@ const PaymentBilling = () => {
 							<h3 className="text-2xl text-black pb-6 w-1/2">Packaging</h3>
 							<h3 className="text-2xl text-black pb-6 w-1/2">Rp 50.000</h3>
 						</div>
-						<Divider custom_divider_color="#FF6C00" className="bottom-[10px] w-full"/>
+						<Divider
+							custom_divider_color="#FF6C00"
+							className="bottom-[10px] w-full"
+						/>
 						<div className="flex flex-row">
 							<h3 className="text-2xl text-black pb-6 w-1/2">Grand Total</h3>
-							<h3 className="text-3xl text-black pb-6 w-1/2 font-bold">Rp 2.702.000</h3>
+							<h3 className="text-3xl text-black pb-6 w-1/2 font-bold">
+								Rp 2.702.000
+							</h3>
 						</div>
 					</div>
 					{/*Payment Detail*/}
 					<div className="p-10 bg-white">
 						<div className="flex flex-row">
-							<h1 className="text-3xl text-black pb-12 w-8/12 font-bold">Payment Detail</h1>
-							<h1 className="text-3xl text-red-500 pb-12 w-4/12 font-bold">11:55:55</h1>
+							<h1 className="text-3xl text-black pb-12 w-8/12 font-bold">
+								Payment Detail
+							</h1>
+							<h1 className="text-3xl text-red-500 pb-12 w-4/12 font-bold">
+								11:55:55
+							</h1>
 						</div>
 						<div className="flex flex-row">
-							<h3 className="text-3x1 text-black pb-6 text-modified">Please make a payment according with the limit time specified, starting from now</h3>
+							<h3 className="text-3x1 text-black pb-6 text-modified">
+								Please make a payment according with the limit time specified,
+								starting from now
+							</h3>
 						</div>
 					</div>
 				</div>
 				{/*Right side*/}
 				<div className="w-1/2 p-10 flex flex-col">
 					<div className="p-10 bg-white">
-						<h1 className="text-3xl text-black pb-12 font-bold">Order Detail</h1>
+						<h1 className="text-3xl text-black pb-12 font-bold">
+							Order Detail
+						</h1>
 						<div className="flex flex-row">
 							<h3 className="text-base text-black pb-6 w-1/2">Order Number</h3>
-							<h3 className="text-base text-black pb-6 w-4/12">MTAWEB-3A86D4DB</h3>
-							<h3 className="text-base text-red-500 pb-6 w-2/12 font-bold">COPY</h3>
+							<h3 className="text-base text-black pb-6 w-4/12">
+								MTAWEB-3A86D4DB
+							</h3>
+							<h3 className="text-base text-red-500 pb-6 w-2/12 font-bold">
+								COPY
+							</h3>
 						</div>
 						<div className="flex flex-row">
 							<h3 className="text-base text-black pb-6 w-1/2">Purchase Date</h3>
-							<h3 className="text-base text-black pb-6 w-1/2">2019-11-07 14:01:48</h3>
+							<h3 className="text-base text-black pb-6 w-1/2">
+								2019-11-07 14:01:48
+							</h3>
 						</div>
 						<div className="flex flex-row">
 							<h3 className="text-base text-black pb-6 w-1/2">Items</h3>
 							<div className="flex flex-col w-1/2">
 								<h3 className="text-base text-black">Way Kambas Mini Ebony</h3>
-								<h3 className="text-base text-gray-400 pb-2">2 x IDR 1.024.000</h3>
+								<h3 className="text-base text-gray-400 pb-2">
+									2 x IDR 1.024.000
+								</h3>
 								<h3 className="text-base text-black">Sikka (Ebony & Mapple)</h3>
-								<h3 className="text-base text-gray-400 pb-6">1 x IDR 1.264.000</h3>
+								<h3 className="text-base text-gray-400 pb-6">
+									1 x IDR 1.264.000
+								</h3>
 							</div>
 						</div>
 						<div className="flex flex-row">
 							<h3 className="text-base text-black pb-6 w-1/2">Name</h3>
-							<h3 className="text-base text-black pb-6 w-1/2">Rasyidin Arsyad Nasution</h3>
+							<h3 className="text-base text-black pb-6 w-1/2">
+								Rasyidin Arsyad Nasution
+							</h3>
 						</div>
 						<div className="flex flex-row">
 							<h3 className="text-base text-black pb-6 w-1/2">Phone</h3>
@@ -80,11 +105,17 @@ const PaymentBilling = () => {
 						</div>
 						<div className="flex flex-row">
 							<h3 className="text-base text-black pb-6 w-1/2">Email</h3>
-							<h3 className="text-base text-black pb-6 w-1/2">rasyid.arsyad@gmail.com</h3>
+							<h3 className="text-base text-black pb-6 w-1/2">
+								rasyid.arsyad@gmail.com
+							</h3>
 						</div>
 						<div className="flex flex-row">
-							<h3 className="text-base text-black pb-6 w-1/2">Shipping Address</h3>
-							<h3 className="text-base text-black pb-6 w-1/2">18 Richardson Drive Fountain Valley, CA 92708</h3>
+							<h3 className="text-base text-black pb-6 w-1/2">
+								Shipping Address
+							</h3>
+							<h3 className="text-base text-black pb-6 w-1/2">
+								18 Richardson Drive Fountain Valley, CA 92708
+							</h3>
 						</div>
 					</div>
 				</div>
@@ -92,22 +123,26 @@ const PaymentBilling = () => {
 
 			<div className="px-10">
 				<div className="bg-white p-10">
-					<h1 className="text-3xl text-black pb-12 font-bold">Payment Method</h1>
+					<h1 className="text-3xl text-black pb-12 font-bold">
+						Payment Method
+					</h1>
 				</div>
 			</div>
 
 			<div className="flex flex-row px-10 py-5">
 				<div className="w-5/12"></div>
 				<Link href={'/'}>
-						<Button ghost className="w-3/12 py-3" htmlType="submit">Continue Shopping</Button>
+					<Button ghost className="w-3/12 py-3" htmlType="submit">
+						Continue Shopping
+					</Button>
 				</Link>
 				<div className="w-1/12"></div>
 				<Link href={'/payment/confirmation'}>
-					<Button className="w-3/12 py-3" htmlType="submit">Place My Order</Button>
+					<Button className="w-3/12 py-3" htmlType="submit">
+						Place My Order
+					</Button>
 				</Link>
 			</div>
-
-
 		</Layout>
 	);
 };
